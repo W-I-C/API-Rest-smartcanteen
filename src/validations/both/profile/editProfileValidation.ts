613a4afa71fecf577c8e3c109f8dcd9d94395262
castro <joaocastro2002@gmail.com>
2022-12-22 10:51:28 +0000

    editProfile fixed

--- src/validations/both/profile/editProfileValidation.ts
+++ src/validations/both/profile/editProfileValidation.ts
@@ -6,21 +6,24 @@ export class EditProfileValidator {
         const queryCampusExists = await checkCampusExistsDBClient.query(`SELECT campusid FROM campus
                                                             WHERE campusid = $1`, [campusId]);
 
-
+        
         const checkBarExistsDBClient = createClient();
         const queryBarExists = await checkBarExistsDBClient.query(`SELECT barid FROM bar
                                                             WHERE barid = $1`, [barId]);
                                                     
-
-        const checkCampusBarDBClient = createClient();
-        const queryCampusBar = await checkCampusBarDBClient.query(`SELECT campus.name, bar.name AS barName
-                                                        FROM campus
-                                                        JOIN bar ON campus.campusid = bar.campusid
-                                                        WHERE bar.campusid = $1 AND bar.barid = "2"`, [campusId, barId]);
-        
         // retorna true ou false
-        return queryCampusExists['rows'].length != 0 && queryBarExists['rows'].length != 0 && queryCampusBar['rows'].length != 0 
+        return queryCampusExists['rows'].length != 0 && queryBarExists['rows'].length != 0
     }
 }
+
+export async function getCampusBar(barId: string) {
+    const getCampusBarDBClient = createClient();
+    const query = await getCampusBarDBClient.query(`SELECT campusid
+                                                    FROM bar
+                                                    WHERE barid = $1`, [barId]);
+
+    return query['rows'][0]['campusid']
+}
+
 
 
