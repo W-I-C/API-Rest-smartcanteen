dfbba1e40fae7404d9b5f745adf6df465f83a0f9
roberto <robertofmbarreto@gmail.com>
2023-01-04 15:21:46 +0000

    middlwares documentation done

--- src/middlewares/isConsumer.ts
+++ src/middlewares/isConsumer.ts
@@ -1,5 +1,15 @@
+/**
+ * @module isConsumer
+ */
 import { Request, Response, NextFunction } from "express"
 
+/**
+ * Consumer role verification middleware
+ * 
+ * @param request request received
+ * @param response response objecet
+ * @param next next function to execute
+ */
 export async function isConsumer(request: Request, response: Response, next: NextFunction) {
   const role = response.locals.role
   if (role === 'consumer') {
