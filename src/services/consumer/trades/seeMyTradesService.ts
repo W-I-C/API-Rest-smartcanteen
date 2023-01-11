edaf721b30243c4560d32e676c20573f06eba156
castro <joaocastro2002@gmail.com>
2023-01-11 16:19:28 +0000

    seeMyTradesService

--- src/services/consumer/trades/seeMyTradesService.ts
+++ src/services/consumer/trades/seeMyTradesService.ts
@@ -7,11 +7,17 @@ export class SeeMyTradesService {
        
         const SeeMyTradesDBClient= createClient();
       
+        // const query=await SeeMyTradesDBClient.query(`SELECT tickets.ticketid, tickets.nencomenda, tickets.ticketamount, tickets.total, states.name
+        //                                         FROM tickettrade 
+        //                                         JOIN tickets ON tickettrade.ticketid = tickets.ticketid
+        //                                         JOIN states ON tickets.stateid = states.stateid
+        //                                         WHERE tickettrade.uid = $1 AND tickettrade.isdeleted = $2 AND tickettrade.isconfirmed = $3 AND tickettrade.receptordecision = $4`,[uId, false, true, 1])
+
         const query=await SeeMyTradesDBClient.query(`SELECT tickets.ticketid, tickets.nencomenda, tickets.ticketamount, tickets.total, states.name
-                                                FROM tickettrade 
-                                                JOIN tickets ON tickettrade.ticketid = tickets.ticketid
-                                                JOIN states ON tickets.stateid = states.stateid
-                                                WHERE tickettrade.uid = $1 AND tickettrade.isdeleted = $2 AND tickettrade.isconfirmed = $3 AND tickettrade.receptordecision = $4`,[uId, false, true, 1])
+                                                 FROM tickets 
+                                                 JOIN tickets ON tickettrade.ticketid = tickets.ticketid
+                                                 JOIN states ON tickets.stateid = states.stateid
+                                                 WHERE tickettrade.uid = $1 AND tickettrade.isdeleted = $2 AND tickettrade.isconfirmed = $3 AND tickettrade.receptordecision = $4`,[uId, false, true, 1])
         
         const data=query["rows"]
 
