be68d5b69e3721bf58b4f20ce9fe4c4c655940fc
castro <joaocastro2002@gmail.com>
2023-01-14 12:27:47 +0000

    fix trades and seeTradePaymentMethod implemented

---
+++ src/services/consumer/trades/seeTradePaymentMethodService.ts
@@ -0,0 +1,24 @@
+
+require('dotenv').config();
+import { createClient } from "../../../config/db";
+import { getDeliveredStatusId, getUserName } from "../../../helpers/dbHelpers";
+import { getUserCampus } from "../../../validations/consumer/trades/seeTradesValidation";
+
+export class SeeTradePaymentMethodService {
+  async execute(uId: string, generalTradeId: string) {
+
+    const seeTradePaymentMethodDBClient = createClient();
+
+    const query = await seeTradePaymentMethodDBClient.query(`SELECT paymentmethodid, paymentmethods.name 
+                                                        FROM generaltrades
+                                                        JOIN paymentmethods ON generaltrades.paymentmethodid = paymentmethods.methodid
+                                                        WHERE generaltradeid = $1`, [generalTradeId])
+
+    const data = query["rows"][0]
+    console.log(query["rows"][0])
+
+    await seeTradePaymentMethodDBClient.end()
+
+    return { data: data, status: 200 }
+  }
+}
