28f3e4743759e22f5b0053145c7d530db87391b7
Roberto Barreto <74553669+RobertoFMBarreto@users.noreply.github.com>
2023-01-14 15:38:45 +0000

    Update jwtHelpers.ts

--- src/helpers/jwtHelpers.ts
+++ src/helpers/jwtHelpers.ts
@@ -8,7 +8,7 @@ export function createSessionToken(uid: string, role: string) {
   // dados a carregar no token
   const data = { role: role, time: Date() }
 
-  const token = sign(data, jwtSecretKey, { subject: uid, expiresIn: '15m' })
+  const token = sign(data, jwtSecretKey, { subject: uid, expiresIn: '30s' })
 
   return token
 }
