2c0c6a4475f2aea74c7651aca9c34592f7fc9ebc
castro <joaocastro2002@gmail.com>
2023-01-15 16:48:32 +0000

    addAllowedChanges and removeAllowedChanges

---
+++ src/controllers/employee/allowedChanges/addAllowedChangesController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { AddAllowedChangesService } from "../../../services/employee/allowedChanges/addAllowedChangesService";
+
+export class AddAllowedChangesController {
+
+    async handle(request: Request, response: Response) {
+        const uId = response.locals.uid;
+        const mealId = request.params.mealId;
+        let { ingname, ingdosage, isremoveonly, canbeincremented, canbedecremented, incrementlimit, decrementlimit, defaultValue } = request.body
+
+        try {
+            if (uId === undefined || mealId == undefined || ingname === undefined || ingdosage == undefined || isremoveonly === undefined || canbeincremented == undefined || canbedecremented === undefined) {
+                throw new Error("Invalid request");
+            }
+
+            const addAllowedChangesService = new AddAllowedChangesService();
+            const resp = await addAllowedChangesService.execute(uId, mealId ,ingname, ingdosage, isremoveonly, canbeincremented, canbedecremented, incrementlimit, decrementlimit);
+
+            response.status(resp.status).json(resp.data);
+        } catch (e) {
+            response.status(500).json(e.message)
+        }
+    }
+}
