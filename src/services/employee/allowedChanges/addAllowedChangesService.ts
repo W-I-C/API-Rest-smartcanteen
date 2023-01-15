7b5760ea93ecb6dea73a976ce76908d4dd59d062
castro <joaocastro2002@gmail.com>
2023-01-15 21:58:03 +0000

    fix

--- src/services/employee/allowedChanges/addAllowedChangesService.ts
+++ src/services/employee/allowedChanges/addAllowedChangesService.ts
@@ -3,7 +3,7 @@ import { createClient } from "../../../config/db";
 
 export class AddAllowedChangesService {
  
-    async execute(uId: String, mealId: String ,ingname: String, ingdosage: String, isremoveonly: Boolean, canbeincremented: Boolean, canbedecremented: Boolean, defaultValue: Number, incrementlimit?: Number, decrementlimit?: Number) {
+    async execute(uId: String, mealId: String ,ingname: String, ingdosage: String, isremoveonly: Boolean, canbeincremented: Boolean, canbedecremented: Boolean, defaultvalue: Number, incrementlimit?: Number, decrementlimit?: Number) {
         const AddAllowedChangesDBClient = createClient();
 
         const nameExists = await AddAllowedChangesDBClient.query(`SELECT *
@@ -14,7 +14,7 @@ export class AddAllowedChangesService {
             throw new Error("This ingredient already exists")
         }
 
-        const remove = await AddAllowedChangesDBClient.query(`INSERT INTO allowedchanges (mealid, ingname,ingdosage,isremoveonly,canbeincremented,canbedecremented,incrementlimit,decrementlimit,defaultvalue) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)`, [mealId,ingname,ingdosage,isremoveonly,canbeincremented,canbedecremented,incrementlimit,decrementlimit,defaultValue]) 
+        const remove = await AddAllowedChangesDBClient.query(`INSERT INTO allowedchanges (mealid, ingname,ingdosage,isremoveonly,canbeincremented,canbedecremented,incrementlimit,decrementlimit,defaultvalue) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)`, [mealId,ingname,ingdosage,isremoveonly,canbeincremented,canbedecremented,incrementlimit,decrementlimit,defaultvalue]) 
         
         console.log("123")
         const query = await AddAllowedChangesDBClient.query(`SELECT *
