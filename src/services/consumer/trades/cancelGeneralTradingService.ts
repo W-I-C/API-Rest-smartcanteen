92353c64642d6f35dcb1cb0fde195b248ea6797c
castro <joaocastro2002@gmail.com>
2023-01-20 15:53:39 +0000

    cancelGeneralTrading fix

--- src/services/consumer/trades/cancelGeneralTradingService.ts
+++ src/services/consumer/trades/cancelGeneralTradingService.ts
@@ -30,28 +30,24 @@ export class CancelGeneralTradingService {
     const campusId = await getUserCampus(uId)
 
     const verifyUser = await cancelTicketTradeDBClient.query(
-      `SELECT tickets.ticketid, tickets.cartid, tickets.emissiondate, tickets.pickuptime, tickets.isfree, tickets.nencomenda, tickets.ticketamount, tickets.total, NULL AS receptordecision, paymentmethods.name AS paymentmethod,  states.name AS statename, ticketowner.name AS ownername, tradereceiver.name AS receivername, true AS isgeneraltrade, NULL AS generaltradeid
-      FROM tickettrade 
+      `SELECT tickets.ticketid, tickets.cartid, tickets.emissiondate, tickets.pickuptime, tickets.isfree, tickets.nencomenda, tickets.ticketamount, tickets.total, tickettrade.receptordecision, paymentmethods.name AS paymentmethod, states.name AS statename, ticketowner.name AS ownername, tradereceiver.name AS receivername, false AS isgeneraltrade, NULL AS generaltradeid
+      FROM tickettrade
       JOIN tickets ON tickettrade.ticketid = tickets.ticketid
-      JOIN bar ON bar.barid = tickets.barid
-      JOIN campus ON campus.campusid = bar.campusid
       JOIN states ON tickets.stateid = states.stateid
       JOIN users ticketowner ON tickets.uid = ticketowner.uid
       JOIN users tradereceiver ON tickettrade.uid = tradereceiver.uid
       LEFT JOIN paymentmethods ON tickettrade.paymentmethodid = paymentmethods.methodid
-      WHERE tickets.uid = $1 
-      AND tickettrade.isdeleted = $2 AND (tickettrade.receptordecision <> 1 OR tickettrade.receptordecision is NULL)
+      WHERE previousowner = $1 
+      AND tickettrade.isdeleted = $2
       UNION
       SELECT tickets.ticketid, tickets.cartid, tickets.emissiondate, tickets.pickuptime, tickets.isfree, tickets.nencomenda, tickets.ticketamount, tickets.total, NULL AS receptordecision, paymentmethods.name AS paymentmethod,  states.name AS statename, ticketowner.name AS ownername, tradereceiver.name AS receivername, true AS isgeneraltrade, generaltrades.generaltradeid
       FROM generaltrades 
       JOIN tickets ON generaltrades.ticketid = tickets.ticketid
-      JOIN bar ON bar.barid = tickets.barid
-      JOIN campus ON campus.campusid = bar.campusid
       JOIN states ON tickets.stateid = states.stateid
       JOIN users ticketowner ON tickets.uid = ticketowner.uid
       LEFT JOIN users tradereceiver ON generaltrades.uid = tradereceiver.uid
       LEFT JOIN paymentmethods ON generaltrades.paymentmethodid = paymentmethods.methodid
-      WHERE tickets.uid = $1 
+      WHERE previousowner = $1 
       AND generaltrades.isdeleted = $2`, [uId, false])
 
     const data = verifyUser["rows"]
